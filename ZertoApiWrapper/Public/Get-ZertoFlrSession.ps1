a39ed0a138d447e676377d8a805ec916a2328cf9
Justin Paul <justin@jpaul.me>
2019-03-24 09:47:02 +0200

    Create Get-ZertoFlrSession.ps1
    First Commit of FLR that gets all sessions

---
+++ ZertoApiWrapper/Public/Get-ZertoFlrSession.ps1
@@ -0,0 +1,59 @@
+<# .ExternalHelp ./en-us/ZertoApiWrapper-help.xml #>
+function Get-ZertoFlrSession{
+    [cmdletbinding( defaultParameterSetName = "main" )]
+    param(
+        [Parameter(
+            ParameterSetName = "filter",
+            HelpMessage = "The VMIdentifier of the VM for which you want to return Jflr Sessions."
+        )]
+        [string]$vmIdentifier,
+        [Parameter(
+            ParameterSetName = "flrId",
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "The identifier or identifiers of the event for which information is returned.")]
+        [string[]]$flrSessionIdentifier
+    )
+
+    begin {
+        $baseUri = "flrs"
+        $returnObject = @()
+    }
+
+    process {
+        # Process based on the ParameterSetName Used
+        switch ( $PSCmdlet.ParameterSetName ) {
+            # If no params are supplied, return all Events
+            "main" {
+                $uri = "{0}" -f $baseUri
+                $returnObject = Invoke-ZertoRestRequest -uri $uri
+            }
+
+            # If one or more flrIdentifiers are supplied, run a foreach loop to get them all
+            "flrId" {
+                $returnObject = foreach ( $id in $eventId ) {
+                    $uri = "{0}/{1}" -f $baseUri, $id
+                    Invoke-ZertoRestRequest -uri $uri
+                }
+            }
+
+            # If a filter is applied, create the filter and return the events that fall in that filter
+            "filter" {
+                $filter = Get-ZertoApiFilter -filterTable $PSBoundParameters
+                $uri = "{0}{1}" -f $baseUri, $filter
+                $returnObject = Invoke-ZertoRestRequest -uri $uri
+            }
+
+            # If a different ParameterSet is called, use the ParameterSet name to determine the URI and call it.
+            default {
+                $uri = "{0}/{1}" -f $baseUri, $PSCmdlet.ParameterSetName.ToLower()
+                $returnObject = Invoke-ZertoRestRequest -uri $uri
+            }
+        }
+    }
+
+    end {
+        return $returnObject
+    }
+}
